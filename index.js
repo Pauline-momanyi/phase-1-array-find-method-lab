2c6a1d7b27533a8a7aece7e1f1fae18ddbda1704
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-11 20:24:26 +0300

    Using find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin (record){
+  const superbowlWinYear = record.find((value)=>value.result === "W"
+)
+//console.log(superbowlWin);
+if  (superbowlWinYear){
+  return superbowlWinYear.year
+}
+}
+console.log(superbowlWin(record))
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -71,3 +71,6 @@ expect(superbowlWin(record)).to.equal('2015');
     expect(superbowlWin(sadReality)).to.equal(undefined)
   });
 });
+
+
+
